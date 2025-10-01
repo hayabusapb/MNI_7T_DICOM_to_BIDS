a52c43bfe1bbd3d6998a5cc76dd865f805e6511b
hayabusapb <95869683+hayabusapb@users.noreply.github.com>
2025-10-01 14:43:14 -0400

    Update map_dicom_series.py
    Suggested wildcard implementation from my branch. Alex

--- src/mni_7t_dicom_to_bids/map_dicom_series.py
+++ src/mni_7t_dicom_to_bids/map_dicom_series.py
@@ -1,6 +1,6 @@
 from mni_7t_dicom_to_bids.dataclass import BidsAcquisitionInfo, DicomBidsMapping, DicomSeriesInfo
 from mni_7t_dicom_to_bids.variables import bids_dicom_ignores, bids_dicom_mappings
-
+import fnmatch # wildcard support line 54 onwards. Alexandre PB
 
 def map_bids_dicom_series(dicom_series_list: list[DicomSeriesInfo]) -> DicomBidsMapping:
     """
@@ -51,7 +51,9 @@ def get_bids_acquisition_info(dicom_series: DicomSeriesInfo) -> BidsAcquisitionI
                 bids_dicom_series_descriptions = [bids_dicom_series_descriptions]
 
             for bids_dicom_series_description in bids_dicom_series_descriptions:
-                if dicom_series.description == bids_dicom_series_description:
+                #if dicom_series.description == bids_dicom_series_description:
+                # replaced by below
+                if fnmatch.fnmatch(dicom_series.description,bids_dicom_series_description):    
                     return BidsAcquisitionInfo(
                         scan_type = bids_scan_type,
                         file_name = bids_file_name,
